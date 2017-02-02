44c227319d05008665f5b2649ec2c18c9b873824
FeastInc <feast.inc.original@gmail.com>
2017-02-02 16:15:14 +0500

    Change script and change resolution
    Изменил скрипт. Сделал тип не по кругу, а просто рандомной движение на
    можно сказать месте и изменил разрешение проекта. Так же создал канвас с
    компонентом Canvas Scale чтобы на разных телефонах норм
    масштабировалось. И воот, затестил на своем телефоне и на мобиле мамы
    все норм работает и показывает, хых

--- project/RealGame/Assets/Scripts/PlanetMove.cs
+++ project/RealGame/Assets/Scripts/PlanetMove.cs
@@ -93,30 +93,59 @@ public class PlanetMove : MonoBehaviour
     private float Speed;
     private Vector2 pointRotation;
     private float radiusRotation;
+    private Vector2 nextPoint;
 
     // Use this for initialization
     void Start()
     {
         position = transform.position;
-        pointRotation = new Vector2(0, 0); //точка вокруг, которой двигаются тела
-        radiusRotation = (float)Math.Sqrt((pointRotation.x - position.x) * (pointRotation.x - position.x) 
+        //pointRotation = new Vector2(0, 0); //точка вокруг, которой двигаются тела
+        //radiusRotation = (float)Math.Sqrt((pointRotation.x - position.x) * (pointRotation.x - position.x) 
             // радиус от точки поворота до центра планеты
-            + (pointRotation.y - position.y) * (pointRotation.y - position.y));
-        angle = (float)Math.Atan2(position.y, position.x); //сначала угол равен не 0, арктагенсу x/y
+            //+ (pointRotation.y - position.y) * (pointRotation.y - position.y));
+        //angle = (float)Math.Atan2(position.y, position.x); //сначала угол равен не 0, арктагенсу x/y
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Math.Cos(angle) == 0 || Math.Sin(angle) == 0)
-            angle += Math.PI / 180;
+        //if (Math.Cos(angle) == 0 || Math.Sin(angle) == 0)
+        //    angle += Math.PI / 180;          
+        
+        if ((Vector2)transform.position == position)
+        {
+            nextPoint = GetNextPoint();
 
-        var nextPoint = new Vector2((float)(radiusRotation * Math.Cos(angle)), 
-            (float)(radiusRotation * Math.Sin(angle)));
-        //считаю следующую точку с помощью параметрического уравнения, где параметр - это угол
+        }
+        else if ((Vector2)transform.position == nextPoint)
+        {
+            nextPoint = position;
+        }     
 
-        transform.position = Vector2.MoveTowards(position, nextPoint, Speed * Time.deltaTime);
-        angle += Speed * Math.PI / 180; // увеличеваю угол, что-то вроде угловой скорости
-        position = transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, nextPoint, Speed * Time.deltaTime);
+        //angle += Speed * Math.PI / 180; // увеличеваю угол, что-то вроде угловой скорости
+        //position = transform.position;
+    }
+
+    private Vector2 GetNextPoint()
+    {       
+        var rnd = new System.Random();
+        var rndNumber = (double)rnd.Next(10, 23) / 100;
+        var rightShift = rndNumber;
+        var upShift = rndNumber;
+
+        var secondRndNumber = rnd.Next(1, 5);
+
+        if (secondRndNumber == 1)
+            rightShift = -rndNumber;
+        else if (secondRndNumber == 2)
+            upShift = -rndNumber;
+        else if (secondRndNumber == 3)
+        {
+            rightShift = -rndNumber;
+            upShift = -rndNumber;
+        }
+
+        return new Vector2((float)(transform.position.x + rightShift), (float)(transform.position.y + upShift));
     }
 }
