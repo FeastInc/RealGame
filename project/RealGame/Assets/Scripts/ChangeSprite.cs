2d74add5bf3626d56873b1834c1cf02fb940b56f
daniel55411 <zhe.dan28@gmail.com>
2017-02-03 20:38:43 +0500

    3_Feb_Daniel

---
+++ project/RealGame/Assets/Scripts/ChangeSprite.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSprite : MonoBehaviour {
+    [SerializeField]
+    private Sprite Sprite;
+
+
+	// Use this for initialization
+	void Start () {
+        var a = GetComponent<SpriteRenderer>();
+        a.sprite = Sprite;  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ project/RealGame/Assets/Scripts/Options/SoundOnOff.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SoundOnOff : MonoBehaviour, IPointerDownHandler {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        var a = GetComponentsInChildren<TextMesh>();
+        if (a[1].text == "Off") a[1].text = "On";
+        else a[1].text = "Off";
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+}
