fa63b030c47a957469c6380d1825334322fb25f3
daniel55411 <zhe.dan28@gmail.com>
2017-02-06 17:24:16 +0500

    6_Feb_Daniel

---
+++ project/RealGame/Assets/Scripts/MainScene/DragStages.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragStages : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    Transform Stages;
+    //private float speed = 22f;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log(eventData.delta.x + "   " + eventData.delta.y);
+        Debug.Log("OnBeginDrag");
+        if (eventData.delta.x != 0)
+        {
+            //var nextPos = new Vector2(eventData.delta.x, StagesTransform.position.y);
+            //Stages.position = Vector2.MoveTowards(StagesTransform.position, nextPos, speed * Time.deltaTime);
+            //Stages.position += new Vector3(eventData.delta.x * 100, 0, 0);
+            transform.position += new Vector3(eventData.delta.x * 10, 0, 0);
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Drag");
+    }
+
+    void Start ()
+    {
+        Stages = transform.GetChild(0);
+    }
+
+	void Update ()
+    {
+        //Debug.Log("Update");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+    }
+}
--- project/RealGame/Assets/Scripts/Options/SoundOnOff.cs
+++ project/RealGame/Assets/Scripts/Options/SoundOnOff.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class SoundOnOff : MonoBehaviour, IPointerDownHandler {
-
 	// Use this for initialization
 	void Start () {
-		
 	}
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        var audio = GetComponentInChildren<AudioSource>();
         var a = GetComponentsInChildren<TextMesh>();
-        if (a[1].text == "Off") a[1].text = "On";
-        else a[1].text = "Off";
+        if (a[1].text == "Off")
+        {
+            a[1].text = "On";
+            audio.Play();
+        }
+        else
+        {
+            a[1].text = "Off";
+            audio.Stop();
+        }
     }
 
 	// Update is called once per frame
